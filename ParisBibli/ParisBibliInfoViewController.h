a3a2133cd2563fe1dbd3824f2b38b481ffae11d2
Alexandre Bulté <alexandre@bulte.net>
2013-02-07 16:00:08 +0100

    Info screen

---
+++ ParisBibli/ParisBibliInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ParisBibliInfoViewController.h
+//  ParisBibli
+//
+//  Created by Alexandre Bulté on 07/02/13.
+//  Copyright (c) 2013 Bulté.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParisBibliInfoViewController : UIViewController
+
+@end
